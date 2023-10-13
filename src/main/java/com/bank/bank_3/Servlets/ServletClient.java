080788ff7942516b2d4bd44ba7aae888261634fe
A.Jelouani <jelouaniabdelghani@gmail.com>
2023-10-13 01:07:47 +0100

    Servlet client et employe craeted

---
+++ src/main/java/com/bank/bank_3/Servlets/ServletClient.java
@@ -0,0 +1,164 @@
+package com.bank.bank_3.Servlets;
+
+
+import com.bank.bank_3.Dto.Client;
+import com.bank.bank_3.Implementation.ImClient;
+import com.bank.bank_3.Services.ServiceClient;
+import jakarta.inject.Inject;
+import jakarta.inject.Named;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+import java.sql.SQLException;
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+@Named
+@WebServlet("/Clients")
+public class ServletClient extends HttpServlet {
+
+    @Inject
+    private ServiceClient serviceClient;
+
+    public ServletClient() throws SQLException {
+        this.serviceClient = new ServiceClient(new ImClient());
+    }
+
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        String action = request.getParameter("action");
+
+        if (action == null) {
+            action = "list";
+        }
+
+        switch (action) {
+            case "edit":
+                editClient(request,response);
+                break;
+            case "update":
+                updateClient(request,response);
+                break;
+            case "add":
+                addClient(request,response);
+                break;
+            case "delete":
+                deleteClient(request,response);
+                break;
+            case "search":
+                chercheClients(request,response);
+                break;
+            default:
+                listClients(request, response);
+        }
+    }
+
+
+    private void listClients(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        Optional<List<Client>> optionalClients = serviceClient.getAllClients();
+        if (optionalClients.isPresent()) {
+            List<Client> clients = optionalClients.get();
+            request.setAttribute("clients", clients);
+        } else {
+            System.out.println("hdgd dgdh");
+        }
+
+        request.getRequestDispatcher("/View/Clients/listeClients.jsp").forward(request, response);
+
+    }
+
+    private void chercheClients(HttpServletRequest request, HttpServletResponse response) throws  ServletException, IOException {
+        String query = request.getParameter("query");
+
+        Optional<Client> searchResult = serviceClient.findClientByEmailOrNomOrPrenom(query);
+
+        if (searchResult.isPresent()) {
+            List<Client> clients = new ArrayList<>();
+            clients.add(searchResult.get());
+            request.setAttribute("clients", clients);
+        } else {
+            System.out.println("Aucun résultat trouvé.");
+        }
+
+        request.getRequestDispatcher("/View/Clients/listeClients.jsp").forward(request, response);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        doGet(request,response);
+    }
+
+
+
+    private void createOrUpdateClient(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        String code = request.getParameter("code");
+        String nom = request.getParameter("nom");
+        String prenom = request.getParameter("prenom");
+        LocalDate datenaissance = LocalDate.parse(request.getParameter("datenaissance"));
+        String telephone = request.getParameter("telephone");
+        String adresse = request.getParameter("adresse");
+
+        Client client = new Client(code, nom, prenom, datenaissance, telephone, adresse);
+
+        String action = request.getParameter("action");
+
+        if ("update".equals(action)) {
+            if (serviceClient.updateClient(client)){
+                response.sendRedirect(request.getContextPath() + "/Clients?action=list");
+            }
+        } else if (serviceClient.saveClient(client).isPresent()) {
+            response.sendRedirect(request.getContextPath() + "/Clients?action=list");
+        } else {
+            response.sendRedirect("/Clients?error=ajoute-failed");
+        }
+    }
+
+    private void addClient(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        createOrUpdateClient(request, response);
+    }
+
+    private void updateClient(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        createOrUpdateClient(request, response);
+    }
+
+
+
+    private void editClient(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        String code = request.getParameter("code");
+        Optional<Client> client = getClientByCode(code);
+        if (client.isPresent()) {
+            request.setAttribute("client", client.get());
+            request.getRequestDispatcher("/View/Clients/editClient.jsp").forward(request, response);
+        } else {
+            response.sendRedirect(request.getContextPath() + "/Clients");
+        }
+    }
+
+    private void deleteClient(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        String code = request.getParameter("code");
+        boolean deleted = serviceClient.deleteClient(code);
+        if (deleted) {
+            response.sendRedirect(request.getContextPath() + "/Clients");
+        } else {
+            response.sendRedirect("/Clients?error=delete-failed");
+        }
+    }
+
+    private Optional<Client> getClientByCode(String code) {
+        if (code != null) {
+            return serviceClient.getClientByCode(code);
+        } else {
+            return Optional.empty();
+        }
+    }
+
+
+
+}
---
+++ src/main/java/com/bank/bank_3/Servlets/ServletEmploye.java
@@ -0,0 +1,154 @@
+package com.bank.bank_3.Servlets;
+
+import com.bank.bank_3.Dto.Employe;
+import com.bank.bank_3.Implementation.ImEmploye;
+import com.bank.bank_3.Services.ServiceEmploye;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+import java.sql.SQLException;
+import java.time.LocalDate;
+import java.util.List;
+import java.util.Optional;
+
+@WebServlet("/Employes")
+public class ServletEmploye extends HttpServlet {
+
+    private final ServiceEmploye serviceEmploye;
+
+
+    public ServletEmploye() throws SQLException {
+        this.serviceEmploye = new ServiceEmploye(new ImEmploye());
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        String action = req.getParameter("action");
+
+        if(action == null){
+            action = "list";
+        }
+
+        switch (action){
+
+            case "edit":
+                editEmploye(req, resp);
+                break;
+            case "update":
+                updateEmploye(req, resp);
+                break;
+            case "add":
+                addEmploye(req, resp);
+                break;
+            case "delete":
+                deleteEmploye(req, resp);
+                break;
+            case "search":
+
+                break;
+            default:
+                listesEmployes(req, resp);
+
+        }
+
+    }
+
+    private void listesEmployes(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        Optional<List<Employe>> optionalEmployes = serviceEmploye.getAllEmployes();
+        if (optionalEmployes.isPresent()){
+            List<Employe> employes = optionalEmployes.get();
+            request.setAttribute("employes", employes);
+        }else{
+            System.out.println("Error!");
+        }
+
+        request.getRequestDispatcher("/View/Employes/listeEmployes.jsp").forward(request,response);
+    }
+
+
+    private void editEmploye(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        String matricule = request.getParameter("matricule");
+        if (matricule != null) {
+            Optional<Employe> employes = serviceEmploye.getEmployeByMatricule(matricule);
+            if (employes.isPresent()) {
+                Employe employe = employes.get();
+                System.out.println("Debug: Found employee for matricule " + matricule);
+                request.setAttribute("employe", employe);
+                request.getRequestDispatcher("/View/Employes/editEmploye.jsp").forward(request, response);
+            } else {
+                response.sendRedirect(request.getContextPath() + "/Employes");
+            }
+        } else {
+            response.sendRedirect(request.getContextPath() + "/Employes");
+        }
+    }
+
+
+    private void deleteEmploye(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        String matricule = request.getParameter("matricule");
+        if (matricule != null) {
+            boolean deleted = serviceEmploye.deleteEmploye(matricule);
+            if (deleted) {
+                response.sendRedirect(request.getContextPath() + "/Employes");
+            } else {
+                response.sendRedirect("/Employes?error=delete-failed");
+
+            }
+        } else {
+            response.sendRedirect(request.getContextPath() + "/Employes");
+        }
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        doGet(request,response);
+    }
+
+    private void addEmploye(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        String matricule = request.getParameter("matricule");
+        String nom = request.getParameter("nom");
+        String prenom = request.getParameter("prenom");
+        LocalDate datenaissance = LocalDate.parse(request.getParameter("datenaissance"));
+        String telephone = request.getParameter("telephone");
+        String adressemail = request.getParameter("adresseEmail");
+        LocalDate daterecrutement = LocalDate.parse(request.getParameter("daterecrutement"));
+
+        Employe employe = new Employe(nom, prenom, datenaissance, telephone,matricule, adressemail,daterecrutement);
+        Optional<Employe> addedClient = serviceEmploye.saveEmploye(employe);
+
+        if (addedClient.isPresent()) {
+            response.sendRedirect(request.getContextPath() + "/Employes?action=list");
+        } else {
+            response.sendRedirect("/Employes?error=ajoute-failed");
+
+        }
+    }
+
+    private void updateEmploye(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        String matricule = request.getParameter("matricule");
+        String nom = request.getParameter("nom");
+        String prenom = request.getParameter("prenom");
+        LocalDate datenaissance = LocalDate.parse(request.getParameter("datenaissance"));
+        LocalDate daterecrutement = LocalDate.parse(request.getParameter("daterecrutement"));
+        String telephone = request.getParameter("telephone");
+        String adresseEmail = request.getParameter("adresseEmail");
+
+
+        Employe employe = new Employe(nom, prenom, datenaissance, telephone,matricule, adresseEmail,daterecrutement);
+
+        boolean updatedClient = serviceEmploye.updateEmploye(employe);
+
+        if (updatedClient) {
+                response.sendRedirect(request.getContextPath() + "/Employes?action=list");
+        } else {
+            response.sendRedirect("/Employes?error=update-failed");
+        }
+    }
+
+
+
+}
